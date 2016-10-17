81c2a97a6508e8da1592a88adfd3a4d97dc13242
Corey <c.brooks96@live.ca>
2016-10-16 22:42:19 -0700

    Initial Commit

---
+++ DataStructs/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  DataStructs
+//
+//  Created by Corey on 16 10, 2016.
+//  Copyright © 2016 Corey. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
